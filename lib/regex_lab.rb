679f7ce80b2871e88683b5d44ce75dddc9233e7a
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 21:21:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  re = /\A[A-Z]+/w+[.!,?;:]\z/
+  if text =~ /\A[A-Z]+\w+[.!,?;:]\z/
 end
 
 def valid_phone_number?(phone)
