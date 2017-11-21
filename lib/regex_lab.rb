61d5dada3cd7a874d193aa0560cbac1f86d8a7d5
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:59:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    # text.scan(/^un.*ing$/)
+    text.scan(/\Aun.*ing\z/)
 end
 
 def words_five_letters_long(text)
