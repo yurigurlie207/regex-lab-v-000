42737d069370aeaf3d5f48fe93c4c78b2097d6f1
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:42:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,6 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
     # text.scan(/^un.*ing$/)
-    ["unerstandig", "understanding", "understanding", "understanding"]
 end
 
 def words_five_letters_long(text)
