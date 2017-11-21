1d7719c3fd685dc86ca5483cf332298c8fded73f
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 21:01:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/[\s]+[a-z]{5}+[\s]/)
+  text.scan(/[\s|^]+[a-z]{5}+[\s]/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
