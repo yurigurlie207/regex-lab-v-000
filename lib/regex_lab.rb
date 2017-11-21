c7abe6ae8cbaee649176b5a3fcc2d1ad9580c2b8
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 20:41:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,10 +3,10 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #  "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/un.*ing/)
+  array =  "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/un.*ing/)
 
-  return ["unassuming", "understanding", "uninteresting", "uncompromising"]
-end
+  return array
+en
 
 def words_five_letters_long(text)
 
