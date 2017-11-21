1c5ca26e8f6e4e90680ac3aea93cc75f2d02e10a
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:31:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^un*ing$/)
-     true
-  else
-    false
-  end
+
 end
 
 def words_five_letters_long(text)
