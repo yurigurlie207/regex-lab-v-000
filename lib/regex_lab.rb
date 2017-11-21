bf920fc7a4d18a02392d6b27dcf94ea7a6411d92
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 21:22:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text =~ /\A[A-Z]+\w+[.!,?;:]\z/
+
+ text =~ /\A[A-Z]+\w+[.!,?;:]\z/ ? true : false
+
 end
 
 def valid_phone_number?(phone)
