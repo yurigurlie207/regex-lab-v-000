81fd1dbbea1d50c7b07eefdcb510cadca1f0333b
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:23:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,7 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiou]/) then true
+  if word.match(/^[aeiou]/)
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
