338d2147b77f6d907bc83b1f4453ad85d56cbb7d
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 20:40:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,8 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   #  "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/un.*ing/)
-  return ["unassuming", "understadning", "uninteresting", "uncompromising"]
+  array = 
+  return ["unassuming", "understanding", "uninteresting", "uncompromising"]
 end
 
 def words_five_letters_long(text)
