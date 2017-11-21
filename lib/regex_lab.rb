8b533aadf9439c244254e3b2b7407f3510b60fbc
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 21:34:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,5 +26,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone =~ /[0-9].*{7}/ ? true : false
+  phone =~ /.*[0-9].*{7}/ ? true : false
 end
