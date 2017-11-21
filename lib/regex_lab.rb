5997ea26869063f523293998a3401dbaa9c2eb69
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 20:26:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ word =~ /\A[aeiou]/i ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return  text.scan(/\Aun.*ing\z/i)
+  text.scan(/\Aun.*ing\z/i)
 end
 
 def words_five_letters_long(text)
