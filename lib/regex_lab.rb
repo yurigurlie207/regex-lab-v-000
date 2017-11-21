3b3d44a6cd9be9a317b954445251354ddb55fe30
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:26:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if word.match(/^un*ing$/)
+     true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
