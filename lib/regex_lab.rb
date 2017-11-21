060f69998d9021044ff0c9b20f7e2472cf6e5170
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 19:41:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    text.scan(/^un.*ing$/)
+    # text.scan(/^un.*ing$/)
     ["unerstandig", "understanding", "understanding", "understanding"]
 end
 
